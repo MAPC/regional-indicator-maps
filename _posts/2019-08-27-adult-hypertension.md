9fe2af1555a532b41aae1c8d49d762509412e6e3
Annabelle Thomas Taylor <ataylor@mapc.org>
2019-08-27 15:54:03 -0400

    Add adult hypertension graph

---
+++ _posts/2019-08-27-adult-hypertension.md
@@ -0,0 +1,6 @@
+---
+layout: post
+title: "Adult Hypertension"
+date: 2019-08-23 11:33:31 -0400
+---
+<svg class="adult-hypertension-map" viewBox="0 0 900 700"></svg>
---
+++ assets/javascripts/adult-hypertension-map.js
@@ -0,0 +1,48 @@
+const hypertensionProjection = d3.geoAlbers()
+  .scale(18000)
+  .rotate([71.057, 0])
+  .center([-0.55, 42.38])
+  .translate([960 / 2, 500 / 2]);
+
+
+function hypertensionHtmlValue(data) {
+    if (data.properties.right_hyp_arte) { return 'Municipality: ' + data.properties.municipal + '<br />' + 'Year: ' + data.properties.right_cal_years + '<br /> Value: ' + data.properties.right_hyp_arte }
+    else { return 'Municipality: ' + data.properties.municipal }
+}
+function hypertensionCreateHeatmap(data) {
+
+  const tooltip = d3.select("body").append("div")
+                   .attr("class", "tooltip")
+                   .style("opacity", 0);
+  const colors = d3.scaleSequentialQuantile()
+      .interpolator(d3.interpolateRdPu)
+      .domain(data.features.map(feature => (feature.properties.right_hyp_arte)));
+  const prematureMortalityMap = d3.select('.adult-hypertension-map');
+  const path = d3.geoPath().projection(hypertensionProjection);
+  prematureMortalityMap.append('g')
+    .attr('class', 'heatmap')
+    .selectAll('path')
+    .data(data.features)
+    .enter()
+    .append('path')
+    .attr('fill', d => {
+      if (!d.properties.right_hyp_arte) { return '#d9d9d9' }
+      else { return colors(d.properties.right_hyp_arte) }})
+    .attr('stroke', '#ffffff')
+    .attr('stroke-width', '1')
+    .attr('stroke-opacity', 0.6)
+    .attr('d', path)
+    .on("mouseover", (d) => {
+        tooltip.transition()
+        .duration(200)
+        .style("opacity", .9);
+        tooltip.html(hypertensionHtmlValue(d))
+        .style("left", (d3.event.pageX) + "px")
+        .style("top", (d3.event.pageY - 28) + "px");
+      }) ;
+}
+
+d3.json('/assets/data/hypertension-hospitalization-rate.geojson').then((data) => {
+  console.log(data);
+  hypertensionCreateHeatmap(data);
+})
